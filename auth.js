d5752dda92afb211c1b818ad30363072faf5e266
yzhbankov <yzhbankov@redpointpositioning.com>
2022-01-13 22:01:17 +0000

    Scan and store contact data once in 10 minutes

--- auth.js
+++ auth.js
@@ -4,22 +4,17 @@ const input = require("input");
 
 const apiId = 14096030;
 const apiHash = "e397be63f6902a716dd689e844c27638";
-const stringSession = new StringSession(""); // fill this later with the value from session.save()
-
+const strSession = "";
 //1AgAOMTQ5LjE1NC4xNjcuNTABu1R3PUo8wiae4XAcyjZasBW1N7wwLchM9rsx0esj2skH+JD5ZSomD0nZ9sAK0SLaWjtJdlbLeICN9WJDl/xINnirzdvGxbezryRWTY7bW0L0SdQn3acxLQ85y8UQcnSH6simjAvKKnFGMEb76k5v6dlmYPrguGF0sH6OOyiOTABp7o5QPUhT/X7VXNCc9WK8XM87WTqL7e0GOhgX6owFrtZOTxaQcOzrAJefhM/ZVkG/SpTFo3w+udO1882m/NRNtv98fRGmk0XZYuvdQxBEPLF7A7slecLkO60GVqg1TY66OGUK5zCplJ9OikoryW1JzStHhP+KfOFSqY8W3VpPsWs=
+const stringSession = new StringSession(strSession); // fill this later with the value from session.save()
+
 
 async function Auth() {
   console.log("Loading interactive example...");
   const client = new TelegramClient(stringSession, apiId, apiHash, {
     connectionRetries: 5,
   });
-  await client.start({
-    phoneNumber: async () => await input.text("Please enter your number: "),
-    password: async () => await input.text("Please enter your password: "),
-    phoneCode: async () =>
-      await input.text("Please enter the code you received: "),
-    onError: (err) => console.log(err),
-  });
+  await client.connect();
   console.log("You should now be connected.");
   console.log(client.session.save()); // Save this string to avoid logging in again
   await client.sendMessage("me", { message: "Hello!" });
--- getStatus.js
+++ getStatus.js
@@ -1,12 +1,37 @@
+const fs = require('fs');
 const { Api, TelegramClient } = require("telegram");
 const { stringSession, Auth, apiHash, apiId } = require("./auth");
-
 const client = new TelegramClient(stringSession, apiId, apiHash, {});
+const CronJob = require('cron').CronJob;
+
+
+async function statuses() {
+  const { users } = await client.invoke(new Api.contacts.GetContacts({}));
+
+  const result = users.map((user) => ({
+    phone: user.phone,
+    name: user.firstName + user.lastName,
+    username: user.username,
+    status: user.status
+  }))
+  const fileName = `statuses_${new Date().toISOString()}.json`;
+
+  fs.writeFile(fileName, JSON.stringify(result), (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully");
+    }
+  }); // prints the result
+}
 
 (async function run() {
   await Auth();
   await client.connect();
-
-  const result = await client.invoke(new Api.contacts.GetStatuses({}));
-  console.log(result); // prints the result
 })();
+
+const job = new CronJob('*/10 * * * *', async function() {
+  await statuses();
+}, null, true, null);
+
+job.start();
