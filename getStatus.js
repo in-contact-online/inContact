d51a7736c66e257721c309bab34c7a096041c328
WhiteAdam <jurchenko.a@gmail.com>
2022-01-14 17:21:25 +0200

    - fixes

--- getStatus.js
+++ getStatus.js
@@ -1,10 +1,20 @@
 const fs = require("fs");
 const { Api, TelegramClient } = require("telegram");
-const { stringSession, Auth, apiHash, apiId } = require("./auth");
-const client = new TelegramClient(stringSession, apiId, apiHash, {});
+const { StringSession } = require("telegram/sessions");
 const CronJob = require("cron").CronJob;
 
+const apiId = 12913782;
+const apiHash = "f801042c2acb3c1a3ab66899bb2e6561";
+const stringSession = new StringSession(
+  "1AgAOMTQ5LjE1NC4xNjcuNTABuzODIMY60dNbPJQ2w0NHePYEup6tRLcuoqMtRK+hBAV8JWTskcVSqtrie/YuKeh2zCkh/u66F3GVSZt/WeYRUo6sK8TvZDglSzn0l3hHts4zmLo996PomMWDDQWS7wBpweh36rL/MV4Xb1ucsoArSdg3AZvQhQxhSm8vnaXd4WQCnUzclsearAJ3MfMdoV9Ip/bWQAYR84YofPPJYWHCvoZPIGSPHGfxyWBQQ2hFTL0ZXDvlaipsSdRO3vr8R4H6Yt+HR3Qdn98lWNxmXF4llqZOYub3yvnQhaSRleNGsribaXNnUiusuMQXVRFVlR3mzxXCqLFDWL/EhL8+Uy6Q4DQ="
+);
+
+const client = new TelegramClient(stringSession, apiId, apiHash, {});
+
 async function statuses() {
+  console.log("Connection...");
+  await client.connect();
+
   const { users } = await client.invoke(new Api.contacts.GetContacts({}));
 
   for (let user of users) {
@@ -22,11 +32,6 @@ async function statuses() {
     );
   }
 }
-
-(async function run() {
-  await Auth();
-  await client.connect();
-})();
 
 const job = new CronJob(
   "*/5 * * * *",
