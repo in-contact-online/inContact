456eff84372b2e7d65dbbd783d421380501b291b
yzhbankov <yzhbankov@redpointpositioning.com>
2022-02-19 17:29:06 +0000

    - trying to add new contacts

---
+++ obsolete/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiId: 12913782,
+    apiHash: 'f801042c2acb3c1a3ab66899bb2e6561',
+    cronString: '*/1 * * * *',
+    sessionId:
+        '1AgAOMTQ5LjE1NC4xNjcuNTABu1XCdj64CVRg30gA23uqmbAh2lgya5gJsMBz8hA7sXPS+LNHfarejU7PWSZmB1qjEC9ig5EiC2viwvttyGvIAz+CblLfneUlqSTUAXicc3eu4pNESpWVNdMO8KTZLzH4afoQW6LnZHnmnoH/dOhgoeeYztVk3gNNAfnr5UoIsI3orZ41RjzkEa+paOQrt95nvi0eJRCcK75XsKIQdFhl5kvXugCDc6ffFLs20OSox+S+cNM5w2shQAgcqOHF7VGFVEqWIyj4TyJwd7CcoEvdN/AfsTqPI+5wLplhykqr6s4p+Eu/Ck3qtbCo3Xho4Z08gp4GiMqccURLuMHKcHTvohw=',
+};
+
+module.exports = {
+    config,
+};
---
+++ obsolete/getStatus.js
@@ -0,0 +1,59 @@
+const { Api, TelegramClient } = require('telegram');
+const { StringSession } = require('telegram/sessions');
+const CronJob = require('cron').CronJob;
+const { config } = require('./config');
+const { getFileName, getFilePath, getUserName, formatUserStatus } = require('./utils/formatUtils');
+const { createFile, appendToFile, fileExist } = require('./utils/fileUtils');
+const fs = require('fs');
+
+const arrayOfNumbers = fs.readFileSync('./userList.txt').toString().split('\n');
+const client = new TelegramClient(new StringSession(config.sessionId), config.apiId, config.apiHash, {});
+
+async function statuses() {
+    console.log('statuses...');
+
+    await client.connect();
+
+    //add contacts
+
+    /*     const mappedArrayOfNumbers = arrayOfNumbers.map((number) => {
+        const generatedId = Math.trunc(Math.random() * 10000000);
+        return new Api.InputPhoneContact({
+            clientId: generatedId,
+            phone: number,
+            firstName: 'name' + generatedId,
+            lastName: 'lastName' + generatedId,
+        });
+    });
+
+    for (let item of mappedArrayOfNumbers) {
+        await client
+            .invoke(
+                new Api.contacts.ImportContacts({
+                    contacts: [item],
+                })
+            )
+            .then((data) => console.log(JSON.stringify(data) + ' added!'))
+            .catch((e) => console.log(e));
+
+        await new Promise((resolve) => {
+            setTimeout(() => resolve(), 20000);
+        });
+    } */
+
+    const { users } = await client.invoke(new Api.contacts.GetContacts({}));
+
+    for (let user of users) {
+        const name = getUserName(user);
+        const filePath = getFilePath(getFileName(getUserName(user)));
+        const status = formatUserStatus(user);
+
+        if (!fileExist(filePath)) {
+            await createFile(filePath, status);
+        } else {
+            await appendToFile(filePath, status);
+        }
+    }
+}
+
+statuses();
