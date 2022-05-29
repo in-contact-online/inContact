f62733903e6682028d0eaa4d86828d03d1ed1a9e
Yaroslav Zhbankov <yzhbankov@redpointpositioning.com>
2022-05-29 19:01:48 +0000

    Cleanup

--- apps/data-scanner/lib/usecases/InitSessionsPool.mjs
+++ apps/data-scanner/lib/usecases/InitSessionsPool.mjs
@@ -2,7 +2,6 @@ import { ClientsPool } from '../models/index.mjs';
 
 export class InitSessionsPool {
     async execute(params) {
-        console.log('InitSessionsPool...');
         return ClientsPool.init(params);
     }
 }
--- apps/data-scanner/lib/usecases/ScanContacts.mjs
+++ apps/data-scanner/lib/usecases/ScanContacts.mjs
@@ -2,7 +2,6 @@ import { ClientsPool } from '../models/index.mjs';
 
 export class ScanContacts {
     async execute(params) {
-        console.log('ScanContacts...');
         //todo: find way how to handle results one by one
         return ClientsPool.checkStatuses();
         // todo: handle result;
