e85b96a21c4597d61bb7269287a36c3f5e7def34
Yaroslav Zhbankov <yzhbankov@redpointpositioning.com>
2022-06-05 17:01:05 +0000

    Add reports table

---
+++ apps/data-scanner/lib/db/migrations/sqls/20220605165421-add-reports-table-up.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS reports;
+CREATE TABLE reports(
+	id SERIAL PRIMARY KEY,
+    phone_number VARCHAR(20) NOT NULL,
+    data TEXT NOT NULL,
+    type VARCHAR(250) NOT NULL,
+    created_at INET NOT NULL
+);
