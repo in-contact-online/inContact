52bb7483650b3fe3fdf327014f099e939f09a03c
WhiteAdam <jurchenko.a@gmail.com>
2022-06-15 10:55:26 +0300

    -fixes

--- apps/monitor-client/lib/app/AppContainer.tsx
+++ apps/monitor-client/lib/app/AppContainer.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 
 import { Login } from '../features/auth/Login';
 import { HomePage } from '../features/HomePage';
 
 export function AppContainer() {
-    return <HomePage />;
+    return (
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<HomePage />} />
+        </Routes>
+    );
 }
 
 AppContainer.defaultProps = {};
