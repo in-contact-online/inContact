f1877744f2ec2262675b30db2c1c81cb8500d96d
WhiteAdam <jurchenko.a@gmail.com>
2022-07-03 10:24:01 +0300

    - fixes

--- apps/monitor-client/lib/features/admin-page/hooks/useFetchData.tsx
+++ apps/monitor-client/lib/features/admin-page/hooks/useFetchData.tsx
@@ -1,34 +1,42 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { dateConvertor, getName } from '../utils';
 import { useQuery } from 'react-query';
 import { AppContext, IAppContext } from '../../../context';
 
-const { api } = useContext<IAppContext>(AppContext);
-
 export function useFetchUsers() {
-    const fetchUsers = (params: any) => api?.users.readList(params);
-    const { data: users, isLoading: isUsersLoading }: any = useQuery('users', () => fetchUsers({ page: 0, size: 5 }), {
-        keepPreviousData: true,
-        select: (users: any) => {
-            return users.data.map((user: any) => {
-                return {
-                    ...user,
-                    username: getName(user.userName, user.firstName, user.secondName),
-                    createdAt: dateConvertor(user.createdAt),
-                    updatedAt: dateConvertor(user.updatedAt),
-                };
-            });
-        },
-    });
+    const { api } = useContext<IAppContext>(AppContext);
+
+    const [paginationCnfg, setPaginationCnfg] = useState({ page: 0, size: 5 });
+
+    const { data: users, isLoading: isUsersLoading }: any = useQuery(
+        ['users', paginationCnfg],
+        () => api?.users.readList(paginationCnfg),
+        {
+            keepPreviousData: true,
+            select: (users: any) => {
+                return users.data.map((user: any) => {
+                    return {
+                        ...user,
+                        username: getName(user.userName, user.firstName, user.secondName),
+                        createdAt: dateConvertor(user.createdAt),
+                        updatedAt: dateConvertor(user.updatedAt),
+                    };
+                });
+            },
+        }
+    );
 
     return { users, isUsersLoading };
 }
 
 export function useFetchContacts() {
-    const fetchContacts = (params: any) => api?.contacts.readList(params);
+    const { api } = useContext<IAppContext>(AppContext);
+
+    const [paginationCnfg, setPaginationCnfg] = useState({ page: 0, size: 5 });
+
     const { data: contacts, isLoading: isContactsLoading }: any = useQuery(
-        'contacts',
-        () => fetchContacts({ page: 0, size: 5 }),
+        ['contacts', paginationCnfg],
+        () => api?.contacts.readList(paginationCnfg),
         {
             keepPreviousData: true,
             select: (contacts: any) => {
