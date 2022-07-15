07287ee9cf931a40a5b48a0f1a74ee9332738012
WhiteAdam <jurchenko.a@gmail.com>
2022-07-15 16:39:29 +0300

    - create landing page

--- apps/monitor-client/lib/features/landing-page/LandingPage.tsx
+++ apps/monitor-client/lib/features/landing-page/LandingPage.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router';
+
+import { LandingMenu } from './components';
 
 import './styles.css';
 
 export function LandingPage() {
-    const navigate = useNavigate();
     return (
         <>
             <div className="first-section dark-background">
                 <div className="container">
                     <div className="header">
                         <div className="logo"></div>
-                        <ul className="menu">
-                            <li onClick={() => navigate('/admin')}>Админка</li>
-                            <li>Возможноти</li>
-                            <li>Тарифы</li>
-                            <li>Кабинет</li>
-                        </ul>
+                        <LandingMenu />
                         <div className="girl"></div>
                         <div className="messages-bg"></div>
                         <div className="light-blur-bg"></div>
