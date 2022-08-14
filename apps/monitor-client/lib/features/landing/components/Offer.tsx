fb43fdc3e1b28a9c8db7e805550156a95c59e4ac
WhiteAdam <jurchenko.a@gmail.com>
2022-08-14 15:36:32 +0300

    - fix offer font-size and position on mobiles

--- apps/monitor-client/lib/features/landing/components/Offer.tsx
+++ apps/monitor-client/lib/features/landing/components/Offer.tsx
@@ -6,8 +6,8 @@ export const Offer = () => {
         <div className="offer">
             <h1 className="offer__header">Контроль статусов Telegram</h1>
             <p className="offer__desc">
-                С помощью нашего telegram-бота вы сможете отслеживать онлайн статус ваших знакомых и понимать с кем они
-                общаются
+                С помощью нашего <strong>telegram-бота</strong> вы сможете отслеживать онлайн статус ваших знакомых и
+                понимать с кем они общаются.
             </p>
             <p className="offer__address">@get_statuses_bot</p>
         </div>
